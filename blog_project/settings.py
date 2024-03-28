47f57cbd7a520c62b0c5d4db4c392b6aa4d3c194
ahmedsamymohamad <ahmed01223330@gmail.com>
2024-03-28 17:57:13 +0200

    primary commit

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -83,14 +83,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blog_project.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 
 AUTH_PASSWORD_VALIDATORS = [
     # {
