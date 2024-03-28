ee15e5fda0a72431ae01c67870f1a57f818072d6
ahmedsamymohamad <ahmed01223330@gmail.com>
2024-03-28 16:33:03 +0200

    primary commit

--- users/models.py
+++ users/models.py
@@ -1,53 +1,55 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
-
-
+from django.utils.translation import gettext_lazy as _
+from django.utils.html import format_html
 from PIL import Image
-from imagekit.models import ImageSpecField
-from imagekit.processors import ResizeToFill,Transpose
+from io import BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
+import sys
 from taggit.managers import TaggableManager
 
+WING_CHOICES = [
+    ('ARMY', _('Army')),
+    ('NAVAL', _('Naval')),
+    ('AIR_FORCE', _('Air Force'))
+]
 
-WING = [
-    
-    ('ARMY','ARMY'),
-    ('NAVAL','NAVAL'),
-    ('AIR FORCE','AIR FORCE')
-    
+REASON_CHOICES = [
+    ('SPAM', _('Spam')),
+    ('INAPPROPRIATE', _('Inappropriate')),
 ]
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=25,blank=True)
-    image = models.ImageField(upload_to='profile_pics',default='profile_pics/avatar.jpg')
-    image_thumbnail_user = ImageSpecField(source='image',
-                                      processors=[Transpose(),ResizeToFill(170, 170)],
-                                      format='JPEG',
-                                      options={'quality': 100})
-
+    name = models.CharField(max_length=25, blank=True)
+    image = models.ImageField(upload_to='profile_pics', default='profile_pics/avatar.jpg')
     description = models.CharField(max_length=100, blank=True)
-    friends = models.ManyToManyField(User,related_name="friends",blank=True)
+    friends = models.ManyToManyField(User, related_name="friends", blank=True)
     camps = TaggableManager(blank=True)
-    wing = models.CharField(max_length=10,choices=WING,blank=True)
+    wing = models.CharField(max_length=10, choices=WING_CHOICES, blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.image:
+            img = Image.open(self.image)
+            if img.height > 170 or img.width > 170:
+                output_size = (170, 170)
+                img.thumbnail(output_size)
+                in_mem_file = BytesIO()
+                img.save(in_mem_file, format='JPEG')
+                img_data = BytesIO(in_mem_file.getvalue())
+                self.image.save(self.image.name, InMemoryUploadedFile(
+                    img_data, None, self.image.name, 'image/jpeg', sys.getsizeof(img_data), None))
+
     def __str__(self):
         return f"{self.user.username}'s Profile"
 
-
-
-REASON = [
-    
-    ('SPAM','SPAM'),
-    ('INAPPROPRIATE','INAPPROPRIATE'),
-    
-]
-
-
 class UserReport(models.Model):
-    reported_user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='reported_user')
-    reason = models.CharField(max_length=13,choices=REASON)
-    reporting_user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='reporting_user')
-    date_reported = models.DateTimeField(default=timezone.now) 
+    reported_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reported_user')
+    reason = models.CharField(max_length=13, choices=REASON_CHOICES)
+    reporting_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reporting_user')
+    date_reported = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.reported_user.username
